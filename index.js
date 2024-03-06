11c136153e7a0cf25afb09167a2ccd8d562b53ef
edougla5 <evandgreat@gmail.com>
2024-03-06 12:03:57 -0500

    adding index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+//message generator program outputs random daily quotes
+let message1 = 'It takes courage to grow up and become who you really are.'
+let message2 = 'Your self-worth is determined by you. You don\'t have to depend on someone telling you who you are.'
+let message3 = 'Keep your face always toward the sunshine, and shadows will fall behind you.'
+
